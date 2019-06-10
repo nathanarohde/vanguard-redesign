401c635837e40497cd2cdd4dbd6970565ba45584
Nathan <nathanarohde@gmail.com>
2019-06-10 16:17:30 -0700

    Update Promo.js

--- src/Components/Promo/Promo.js
+++ src/Components/Promo/Promo.js
@@ -11,7 +11,7 @@ const promo = () => {
           The next evolution of ESG investing.
         </h2>
         <p>
-          <b>Vanguard Global ESG Select Stock Fud</b> is the first actively managed fund in our environmental, social, and governance (ESG) lineup. It includes stocks from financially sound companies with the highest standards for conducting business in support of ESG issues.  You can get started today with as little as $3,000.
+          <b>Vanguard Global ESG Select Stock Fund</b> is the first actively managed fund in our environmental, social, and governance (ESG) lineup. It includes stocks from financially sound companies with the highest standards for conducting business in support of ESG issues.  You can get started today with as little as $3,000.
         </p>
         <p className="bodyLinks">
           <a href="https://investor.vanguard.com/investing/esg/">See what makes this new fund different</a>
