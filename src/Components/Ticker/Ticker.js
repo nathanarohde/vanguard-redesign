e49c47e24eefd9f6cf369e2980f1a46a926865fb
Nathan <nathanarohde@gmail.com>
2019-06-10 15:21:25 -0700

    ticker content block in

--- src/Components/Ticker/Ticker.js
+++ src/Components/Ticker/Ticker.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import './Ticker.css';
 
 const ticker = () => {
   return (
-    <section>
-      <p>
-        Ticker stuff.
+    <section className="ticker">
+      <p className="tickerWrapper">
+        <span>Nasdaq |values|</span>
+        <span>S&P |values|</span>
+        <span>10-yr T-note |values|</span>
+        <span>DIJA |values|</span>
       </p>
     </section>
   )
