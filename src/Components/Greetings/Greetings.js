23606b3735c829f75ac0b5433065b90c6dae08c9
Nathan <nathanarohde@gmail.com>
2019-06-10 14:52:48 -0700

    Greetings section block-in

--- src/Components/Greetings/Greetings.js
+++ src/Components/Greetings/Greetings.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import './Greetings.css';
 
 const greetings = () => {
   return (
-    <section>
+    <section className="greetings">
+      <h2>Welcome back, |firstName|</h2>
       <p>
-        Greetings stuff.
+        <span><b>Email: </b>nathanarohde@gmail.com <b>edit </b></span>
+        <span><b>Last logon: </b>| date | time | time zone |</span>
       </p>
     </section>
   )
