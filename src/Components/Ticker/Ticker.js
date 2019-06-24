9bed700d9308983ee70fb94e3bacac9401a0a525
Nathan <nathanarohde@gmail.com>
2019-06-24 12:07:31 -0700

    program can now access stock market api
    Api key is ignored.

--- src/Components/Ticker/Ticker.js
+++ src/Components/Ticker/Ticker.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import './Ticker.css';
 
-const ticker = () => {
+const ticker = (props) => {
   return (
     <section className="ticker">
       <p className="tickerWrapper">
-        <span>Nasdaq |values|</span>
-        <span>S&P |values|</span>
-        <span>10-yr T-note |values|</span>
-        <span>DIJA |values|</span>
+        <span>Nasdaq {props.nasdaq.data['Global Quote']["05. price"]}</span>
+        <span>S&P {props.sp500.data['Global Quote']["05. price"]}</span>
+        <span>10-yr T-note {props.tnote.data['Global Quote']["05. price"]}</span>
+        <span>DJIA {props.djia.data['Global Quote']["05. price"]}</span>
       </p>
     </section>
   )
--- src/Containers/Main/Main.js
+++ src/Containers/Main/Main.js
@@ -1,14 +1,45 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import Ticker from '../../Components/Ticker/Ticker';
 import Greetings from '../../Components/Greetings/Greetings';
 import Promo from '../../Components/Promo/Promo';
 import FrequentLinks from '../../Components/FrequentLinks/FrequentLinks';
+import keys from '../../key.json';
 
 class Main extends Component {
+  state = {
+    djia: null,
+    nasdaq: null,
+    sp500: null,
+    tnote: null,
+  }
+
+  componentDidMount() {
+    for (let stock in this.state.stockList) {
+      console.log(this.state.stockList[stock]);
+    }
+    // this.setTicker('djia', 'DJIA');
+    // this.setTicker('nasdaq', 'NDAQ');
+    // this.setTicker('sp500', '.INX');
+    // this.setTicker('tnote', '^TNX');
+  }
+
+  setTicker(name, ticker){
+    axios.get('https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=' + ticker + '&apikey='+ keys.alphavantage)
+      .then( response => {
+        this.setState({ [name]:response });
+      })
+      .catch( error => {
+        console.log('Error');
+      })
+  }
+
   render() {
     return (
       <main>
-        <Ticker></Ticker>
+        { (this.state.djia && this.state.nasdaq && this.state.sp500 && this.state.tnote) &&
+          <Ticker djia={this.state.djia} nasdaq={this.state.nasdaq} sp500={this.state.sp500} tnote={this.state.tnote}></Ticker>
+        }
         <Greetings></Greetings>
         <Promo></Promo>
         <p>Here is body content.</p>
